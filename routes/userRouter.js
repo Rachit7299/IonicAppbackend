1fabbd9cc2bae478e548fc2abe35a3634287714c
amolsr <amol.saini567@gmail.com>
2020-09-15 19:49:36 +0530

    duplicate error

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -15,30 +15,23 @@ userRouter.route('/')
     .catch((err)=>next(err));
 });
 
-userRouter.route('/signup')
-.post((req,res,next)=>{
-    users.findOne({mobile: req.body.mobile})
-    .then((user)=>{
-        if(user!=null){
-            var err = new Error('User ' + req.body.name + ' already exists!');
-            err.status = 403;
-            return next(err);
-        }
-        else{
-            return users.create({
-                name:req.body.name,
-                email:req.body.email,
-                mobile:req.body.mobile,
-                city:req.body.city,
-                pswd:req.body.pswd,
-            })
-        }
-    }).then((user)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({status: 'Registration Successful!', user: user});
-    },(err)=>next(err))
-    .catch((err)=>next(err));
+userRouter.route("/signup").post((req, res, next) => {
+  users
+    .create({
+      name: req.body.name,
+      email: req.body.email,
+      mobile: req.body.mobile,
+      city: req.body.city,
+      pswd: req.body.pswd,
+    })
+    .then((user) => {
+      res.status(200).json({ status: "Registration Successful!", user: user });
+    })
+    .catch((err) => {
+      if (err.code === 11000)
+        res.status(409).json({ status: "Duplicate Entry Found" });
+      else res.status(500).json({ status: "Internal Server" });
+    });
 });
 
 module.exports=userRouter;
