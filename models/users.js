b930e4debd35e3fb9fe36ff3814d10a609b9b76a
Rachit7299 <itssunil.agarwal@gmail.com>
2020-10-02 20:18:06 +0530

    'address-model-changed'

--- models/users.js
+++ models/users.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const addressSchema = new Schema({
+    address_name:{
+        type:String,
+        required: true
+    },
     address:{
         type:String,
         required: true
